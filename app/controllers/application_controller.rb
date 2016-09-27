24763b80b86ed4bd68b21cdb59b50c919ad7b62a
Victor Fernandes <victor.hmfd@gmail.com>
2016-09-27 16:58:04 -0300

    configure internacionalization
     -create method for loading locale via url
     -create pt-Br locale
     -adding internacionalization for navbar
     -add links in footer for translation
    
    Signed-off-by: Mateus Furquim <furquim.axs@gmail.com>
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   private
 
   def current_user
