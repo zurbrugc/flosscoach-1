7502f4807b3101f43aad7a2affc52b7f853bedb7
Filipe Ribeiro <filiperibeirom7@gmail.com>
2016-10-13 17:20:02 -0300

    correct operational system to operating system

--- db/migrate/20160526182007_add_operational_system_id_to_project.rb
+++
@@ -1,6 +0,0 @@
-class AddOperationalSystemIdToProject < ActiveRecord::Migration
-  def change
-    add_column :projects, :operational_system_id, :integer
-    add_index  :projects, :operational_system_id
-  end
-end
