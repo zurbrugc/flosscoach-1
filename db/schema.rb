33897741ed11d11d87bd86db55ac424d946ead73
Orochi <imatheusfsantos@gmail.com>
2016-10-18 01:47:14 -0200

    add activeadmin files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928050449) do
+ActiveRecord::Schema.define(version: 20161018034110) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "languages", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +34,7 @@ ActiveRecord::Schema.define(version: 20160928050449) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "operational_systems", force: :cascade do |t|
+  create_table "operating_systems", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
