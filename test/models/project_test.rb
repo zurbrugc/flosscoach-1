b710f27989f102b352ab5db00d8d0f84b0c9920e
Orochi <imatheusfsantos@gmail.com>
2017-02-11 19:43:16 -0200

    refactor and create news tests for owners/admin in projects

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  test "the truth" do
-    build(:project)
-    assert true
+  test "user creating a project" do
+    user = create(:user)
+    project = create(:project)
+    project.owners << user
+
+    assert_equal 1, project.owners.size, "User project count is different of one"
+    assert_equal project, user.projects.first
+    assert_equal user, project.owners.first
+  end
+
+  test "user removed from administration of a project" do
+    user = create(:user)
+    project = create(:project)
+    project.owners << user
+    assert_equal 1, project.owners.size, "User project count is different of one"
+
+    project.owners.delete(user)
+    assert_equal 0, project.owners.size, "User project count is different of zero"
+    assert_equal 0, user.projects.size
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,25 +6,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save, "User with invalid email has been saved!"
   end
 
-  test "user creating a project" do
-    user = create(:user)
-    project = create(:project)
-    project.owners << user
 
-    assert_equal 1, project.owners.size, "User project count is different of one"
-    assert_equal project, user.projects.first
-  end
-
-  test "user removed from administration of a project" do
-    user = create(:user)
-    project = create(:project)
-    project.owners << user
-    assert_equal 1, project.owners.size, "User project count is different of one"
-
-    project.owners.delete(user)
-    assert_equal 0, project.owners.size, "User project count is different of zero"
-    assert_equal 0, user.projects.size
-  end
 
   test "generate slug from username" do
     user = build(:user)
