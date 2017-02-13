2f55bc4d057184c7783b72d054c56eb3b55ac2aa
Orochi <imatheusfsantos@gmail.com>
2017-02-13 20:27:16 -0200

    edit model

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,6 +9,8 @@ class Project < ApplicationRecord
   has_many :favoriter_projects
   has_many :fans, through: :favoriter_projects, :source => :user
   has_many :comments, through: :widgets
+  mount_uploader :avatar, AvatarUploader
+  
   def self.search(search)
     if search
       Project.where('name LIKE ?', "%#{search}%")
