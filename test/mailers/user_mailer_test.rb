fd66aeeb58cb5558dd522c3e145c14b103ed10f3
Orochi <imatheusfsantos@gmail.com>
2017-02-20 11:18:19 -0300

    add or remove files

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+class UserMailerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @emails = ActionMailer::Base.deliveries
+    @emails.clear
+  end
+
+  test "send email for activation on register" do
+    user_attributes = attributes_for(:user)
+    post users_url, params: {user: user_attributes}
+    assert_redirected_to root_url
+
+    mail = @emails.last
+    assert_equal user_attributes[:email], mail.to.first
+
+  end
+end
