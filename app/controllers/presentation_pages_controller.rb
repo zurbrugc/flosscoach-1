6975217b147294275b4ce0c5713928deefcf87ca
Orochi <imatheusfsantos@gmail.com>
2017-04-06 19:51:30 -0300

    add files

---
+++ app/controllers/presentation_pages_controller.rb
@@ -0,0 +1,17 @@
+class PresentationPageController < ApplicationController
+
+  # GET /presentation_pages
+  def index
+  end
+
+  # GET /presentation_pages/1
+  def show
+  end
+
+
+  # GET /presentation_pages/1/edit
+  def edit
+  end
+
+
+end
