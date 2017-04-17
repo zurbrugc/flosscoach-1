86cf6e9c5e4aa7954a542c2d2638992333a56a48
Orochi <imatheusfsantos@gmail.com>
2017-04-17 19:46:12 -0300

    adjusts in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
   # Generate digests for assets URLs
