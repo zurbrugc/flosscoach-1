a533b43ebd5aefffa79a6ffd44b1215e4dec9caf
Orochi <imatheusfsantos@gmail.com>
2017-04-25 13:50:35 -0300

    trying to solve production problem with assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,19 +26,17 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
