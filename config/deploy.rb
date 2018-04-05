a5c2fc6ad9aaea4e6dcde6fbf8ee34ba48889f38
Henrique Sousa <rickftime@gmail.com>
2018-04-05 17:03:13 +0100

    Set up puma, nginx and capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,8 +32,7 @@ set :migration_role, :app
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml config/secrets.yml}
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+#set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public}
 
 namespace :puma do
