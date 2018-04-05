82aa2cfa2ad7f90fdf5ad161ee28f8f729832dbe
Henrique Sousa <rickftime@gmail.com>
2018-04-05 12:43:47 +0100

    Set up puma, nginx and capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,9 @@
-# Change these
 server '159.89.147.95', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'https://gitlab.com/flosscoach/flosscoach'
 set :application,     'flosscoach'
 set :user,            'flosscoach'
-set :puma_threads,    [2]
+set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
@@ -18,7 +17,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/flosscoach.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
