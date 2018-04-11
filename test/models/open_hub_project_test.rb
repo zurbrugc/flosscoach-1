0c512200fff27dc573f5288189a98a22d2bbb2c9
Marcelo Almeida <jmfda00@gmail.com>
2018-04-11 10:42:11 -0300

    Updating Firefox Project Homepage URL on test_find_firefox_project_by_name

--- test/models/open_hub_project_test.rb
+++ test/models/open_hub_project_test.rb
@@ -4,12 +4,14 @@ class OpenHubProjectTest < ActiveSupport::TestCase
   test "find firefox project by name" do
     project =  OpenHubProject.find_by_name("Firefox")
     assert_equal "Mozilla Firefox", project.name
-    assert_equal "http://www.firefox.com/", project.homepage_url
+    #assert_equal "http://www.firefox.com/", project.homepage_url
+    assert_equal "https://www.mozilla.org/en-US/firefox/", project.homepage_url
   end
 
   test "find firefox project by id" do
     project =  OpenHubProject.find_by_id(9) #9 is the ID of Firefox Project on OpenHub
     assert_equal "Mozilla Firefox", project.name
-    assert_equal "http://www.firefox.com/", project.homepage_url
+    #assert_equal "http://www.firefox.com/", project.homepage_url
+    assert_equal "https://www.mozilla.org/en-US/firefox/", project.homepage_url
   end
 end
