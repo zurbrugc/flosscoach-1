4bfddd7813f635b9e1ba1097ba23e7587769f3e8
Marcelo <jmfda00@gmail.com>
2018-06-06 23:48:14 -0300

    changing github oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,9 @@
-opts = { scope: 'user:email' }
-
 Rails.application.config.middleware.use OmniAuth::Builder do
 
     provider :facebook, "#{Rails.application.secrets.FACEBOOK_ID}", "#{Rails.application.secrets.FACEBOOK_KEY}",
     :scope => "email, public_profile"
-    provider :twitter, Rails.application.secrets.TWITTER_ID, Rails.application.secrets.TWITTER_KEY, opts
-    provider :github, Rails.application.secrets.GITHUB_ID, Rails.application.secrets.GITHUB_KEY, opts
+    provider :twitter, Rails.application.secrets.TWITTER_ID, Rails.application.secrets.TWITTER_KEY
+    provider :github, Rails.application.secrets.GITHUB_ID, Rails.application.secrets.GITHUB_KEY
 
 end
 
